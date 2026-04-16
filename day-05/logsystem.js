46b0437382f03d7293c488a4a188c356089c8875
Rajneesh Chaubey <pinea8888@gmail.com>
2026-04-16 11:15:45 +0530

    day 12

--- day-05/logsystem.js
+++
@@ -1,9 +0,0 @@
-function logMessage(level, message) {
-    const time = new Date().toISOString();
-    console.log(`[${time}] [${level}] ${message}`);
-}
-
-// Usage
-logMessage("INFO", "User clicked button");
-logMessage("ERROR", "Something went wrong");
-logMessage("WARNING", "Low battery");
